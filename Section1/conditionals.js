9102d94e51d9106e7cf84969a8472ac75d49a868
EdwardWong102 <95390570+EdwardWong102@users.noreply.github.com>
2021-12-08 17:23:47 -0800

    Add files via upload

---
+++ Section1/conditionals.js
@@ -0,0 +1,15 @@
+// if, else if, else, statements
+
+let age = 32;
+if (age >= 5 && age <12){
+    console.log("you must be in elementary")
+}
+else if (age >= 12 && age <18){
+    console.log("you must be in highschool")
+}
+else if (age >= 18 && age < 25){
+    console.log("you must be a college student")
+}
+else{
+    console.log("imagine not being in school LLLLLLLL")
+}
