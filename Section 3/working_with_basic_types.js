39b46f8846d2149ed652dd59846787638d4e244f
EdwardWong102 <95390570+EdwardWong102@users.noreply.github.com>
2021-12-12 23:24:20 -0800

    Add files via upload

---
+++ Section 3/working_with_basic_types.js
@@ -0,0 +1,23 @@
+let org = "C.C.P.";
+let country = "Chinese";
+let mc = "John Xina";
+let food = "Bing QiLin";
+
+let socialCreditScore = 104123540;
+let newSocialCreditScore = -139945712;
+let ammountOfIcecream = 21;
+
+let flavorsOfIcecream = ['vanilla', 'chocolate', 'strawberry', 'mint chocolate chip', 'rocky road', 'yuzu sorbet'];
+//only real ones eat mint chocolate
+
+let taiwan = true;
+
+console.log("Hello my name is " + mc)
+console.log("I love " + food)
+console.log("I like " + flavorsOfIcecream[1] + " flavored " + food + " I have " + ammountOfIcecream + " gallons of " + food)
+console.log("I am currently on the run from the " + country + " govenment, the " + org)
+console.log("I used to have a social credit score of " + socialCreditScore + " because of all the " + country + " propaganda i spread") 
+console.log("Then, out of nowhere, a " + org + " agent asked me if taiwan was a country")
+console.log("I replied yes, its " + taiwan + " Taiwan is a country")
+console.log("The " + org + " agent then told me that my social credit score was now " + newSocialCreditScore)
+console.log("the " + org + " agent also told me that i have a ten second headstart time before the " + org + " will come to publicly execute me")
